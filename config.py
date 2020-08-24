dff3cc582dee17a5cb19210060c105c7ef4fc2d6
abhijit shaw <36149425+shaw2018@users.noreply.github.com>
2020-08-24 21:52:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 """ Configuration file """
 
-TOKEN = ''  # Your bot token
-DEFAULT_PREFIX = 'm.'  # Bot prefix
-LANGUAGE = 'ru_RU'  # Bot language. Avialable: ru_RU, en_EN
+TOKEN = 'NzQ3NDkwOTA2MTk3ODUyMjYx.X0PpJw.4kK7VlUYjy7HfqL-pzosoXK3f_E'  # Your bot token
+DEFAULT_PREFIX = 'b.'  # Bot prefix
+LANGUAGE = 'en_EN'  # Bot language. Avialable: ru_RU, en_EN
